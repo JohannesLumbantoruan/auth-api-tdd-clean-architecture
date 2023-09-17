42d206e8e2a42a3e80a15eaaf30f90b6521f5096
Johannes Lumbantoruan <johannestrikardo@gmail.com>
2023-09-18 04:37:56 +0700

    First commit

---
+++ src/Interfaces/http/api/users/index.js
@@ -0,0 +1,12 @@
+const UsersHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+    name: 'users',
+    version: '1.0.0',
+    register: async (server, { container }) => {
+        const usersHandler = new UsersHandler(container);
+
+        server.route(routes(usersHandler));
+    }
+};
